450db3d043305f259f05cadedcd6ee765e1c4a81
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-11-14 12:56:53 -0500

    Add: environment variable to server URL

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
-const socket = io.connect('http://localhost:2000');
+const socket = io.connect(process.env.REACT_APP_SERVER_URL);
 
 function App() {
   const [messagee, setMessagee] = useState('');
