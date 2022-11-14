69da0269e87baf51223c03a7bb328d0d2aadc040
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-11-14 12:49:03 -0500

    Add: basic styles and set state with messages from sockets

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,48 @@
+import { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
 const socket = io.connect('http://localhost:2000');
 
 function App() {
+  const [messagee, setMessagee] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessages([...messages, messagee]);
+    socket.emit('client:sentmessage', messagee);
+    setMessagee('');
+  };
+
+  useEffect(() => {
+    socket.on('server:sendmessage', (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
   return (
     <div className='App'>
-      <h1>Hellooo</h1>
+      <header>
+        <h1>Hellooo</h1>
+        <p>Please enter your message</p>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder='Hola mundo'
+            value={messagee}
+            onChange={(e) => setMessagee(e.target.value)}
+          />
+          <button type='submit'>Submit</button>
+        </form>
+      </header>
+      <main>
+        <ul>
+          {messages.map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
